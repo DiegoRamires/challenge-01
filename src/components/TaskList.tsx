241d5efdd5a850a4f86175fb67a5a82c65f5e507
Diego Ramires <goncalves.ramires@gmail.com>
2021-03-13 23:45:36 -0300

    feature: toggle isComplete tasks added action

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -32,7 +32,12 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksAdded = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task)
 
+    setTasks(tasksAdded)
   }
 
   function handleRemoveTask(id: number) {
